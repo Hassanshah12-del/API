0cbf7854757c461d30126a7110ced6e631a6761e
Hassanshah12-del <58622810+Hassanshah12-del@users.noreply.github.com>
2023-09-13 16:36:29 +0500

    Add files via upload

---
+++ tasks/tasks.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task, TaskStatus } from './task.model';
+
+@Controller('tasks')
+export class TasksController {
+    constructor(private taskService:TasksService){}
+        @Get()
+        getALLTasks():Task[]{
+            return this.taskService.getAllTask()
+        }
+        @Get('/:id')
+        getTaskById(@Param('id')id:string):Task{
+            return this.taskService.getTaskById(id);
+        }
+@Post()
+createTask(@Body('title')title:string,
+@Body('description')description:string):
+Task{
+    return this.taskService.createTask(title,description);
+}
+    
+@Delete('/:id')
+deleteTask(@Param('id')id:string){
+    this.taskService.deleteTask(id);
+    return 'task $(id) is deleted'
+}
+@Patch('/:id')
+updateTaskStatus(@Param('id')id:string,
+@Body('status')status:TaskStatus){
+    return this.taskService.updateTaskStatus(id,status)
+}
+}
